fab9be59d1dfd2c69f26392b2a2246097b435a5a
Murat Ozcan <muratkerem@gmail.com>
2024-08-26 08:18:32 -0500

    improved env setup

--- scripts/env-setup.sh
+++ scripts/env-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-# Load environment variables
-set -a # all variables that are subsequently defined are automatically exported to the environment of subsequent commands
-source .env
-set +a # turns off allexport
+# Load environment variables from .env if it exists
+if [ -f .env ]; then
+  set -a # all variables that are subsequently defined are automatically exported to the environment of subsequent commands
+  source .env
+  set +a # turns off allexport
+fi
 
 # Set Git-related environment variables
 export GITHUB_SHA=$(git rev-parse --short HEAD)
