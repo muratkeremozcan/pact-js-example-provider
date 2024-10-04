19e8488e352af6593176efb96eb078669434f7dd
Murat Ozcan <muratkerem@gmail.com>
2024-10-04 12:27:21 -0500

    experiment: new get response

--- src/@types/schema.ts
+++ src/@types/schema.ts
@@ -31,7 +31,7 @@ export const CreateMovieResponseSchema = z
       .number()
       .int()
       .openapi({ example: 200, description: 'Response status code' }),
-    movie: z
+    data: z
       .object({
         id: z.number().openapi({ example: 1, description: 'Movie ID' }),
         name: z
@@ -58,16 +58,35 @@ export const ConflictMovieResponseSchema = z.object({
 })
 
 const movieObj = {
-  id: z.number(),
-  name: z.string(),
-  year: z.number()
+  id: z.number().openapi({ example: 1, description: 'Movie ID' }),
+  name: z.string().openapi({ example: 'Inception', description: 'Movie name' }),
+  year: z.number().openapi({ example: 2010, description: 'Release year' })
 }
+
 export const GetMovieResponseUnionSchema = z
-  .union([
-    // Use union to handle both single and array responses
-    z.object(movieObj).nullable(),
-    z.array(z.object(movieObj))
-  ])
+  .object({
+    status: z
+      .number()
+      .int()
+      .openapi({ example: 200, description: 'Response status code' }),
+    data: z.union([
+      z
+        .object(movieObj)
+        .nullable()
+        .openapi({
+          description: 'Movie details or null if not found',
+          example: { id: 1, name: 'Inception', year: 2010 }
+        }),
+      z.array(z.object(movieObj)).openapi({
+        description: 'List of movies or an empty array if no movies exist',
+        example: []
+      })
+    ]),
+    error: z.string().nullable().openapi({
+      description: 'Error message if an error occurred, otherwise null',
+      example: null
+    })
+  })
   .openapi('GetMovieResponse')
 
 export const MovieNotFoundResponseSchema = z.object({
@@ -117,7 +136,7 @@ export const UpdateMovieResponseSchema = z
       .number()
       .int()
       .openapi({ example: 200, description: 'Response status code' }),
-    movie: z
+    data: z
       .object({
         id: z.number().openapi({ example: 1, description: 'Movie ID' }),
         name: z
