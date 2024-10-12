9ee7434f88dea32896ecf4b4e811fe455c29a14a
Murat Ozcan <muratkerem@gmail.com>
2024-10-12 06:37:59 -0500

    feat: kafka part 2

--- src/events/movie-events.ts
+++ src/events/movie-events.ts
@@ -17,21 +17,36 @@ const kafka = new Kafka({
 })
 const producer = kafka.producer()
 
-export const produceMovieEvent = async (movie: Movie) => {
-  const event = {
-    topic: 'movie-created',
+type MovieAction = 'created' | 'updated' | 'deleted'
+type Event<T extends string> = {
+  topic: `movie-${T}`
+  messages: Array<{
+    key: string // id as string
+    value: string // serialized movie object
+  }>
+}
+type MovieEvent = Event<MovieAction>
+
+// console log it and write the event to a file, so we can somewhat verify them
+// in the real world, you might check db, other services, or any other external side effects
+const logEvent = (event: MovieEvent) => {
+  console.table(event)
+  fs.appendFileSync(logFilePath, `${JSON.stringify(event)}\n`)
+}
+
+export const produceMovieEvent = async (
+  movie: Movie,
+  action: 'created' | 'updated' | 'deleted'
+) => {
+  const event: MovieEvent = {
+    topic: `movie-${action}`,
     messages: [{ key: movie.id.toString(), value: JSON.stringify(movie) }]
   }
 
   try {
     await producer.connect()
     await producer.send(event)
-
-    // console log it and write the event to a file, so we can somewhat verify them
-    // in the real world, you might check db, other services, or any other external side effects
-    console.table(event)
-    fs.appendFileSync(logFilePath, `${JSON.stringify(event)}\n`)
-
+    logEvent(event)
     await producer.disconnect()
   } catch (err) {
     console.error(
